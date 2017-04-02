ac17786cb1c9ae0c9779ac0b4124f59b3921c0a1
Joshua Boren <joshua.boren@borenlabs.com>
2017-04-02 15:07:18 -0700

    removed some old code from XML days

--- components/bugstructure.php
+++ components/bugstructure.php
@@ -64,40 +64,6 @@ class DarkBuglistIO
 		}
 		$this->fixlist($buglist);
 
-		/*else if ( file_exists(__DIR__ . "/" . $filename . ".xml") )
-		{
-			$xml = new DOMDocument();
-			$xml->load( __DIR__ . "/" . $filename  . ".xml");
-
-			$x = $xml->documentElement;
-			foreach ( $x->childNodes AS $item )
-			{
-				$bug = new DarkBug();
-				$bug->title = $item->getAttribute( "title" );
-				$bug->status = $item->getAttribute( "status" );
-				foreach ( $item->childNodes AS $props )
-				{
-					if ( $props->nodeName == "description" ) {
-						$bug->description = $props->nodeValue;
-					}
-					else if ( $props->nodeName == "history" ) {
-						foreach ( $props->childNodes AS $pasts )
-						{
-							array_push( $bug->history, $pasts->nodeValue );
-						}
-					}
-				}
-				if ( $item->hasAttribute( "id" ) ) {
-					$bug->id = $item->getAttribute( "id" );
-				}
-				else {
-					$bug->id = $lastId;
-					$lastId += 1;
-				}
-				array_push( $buglist, $bug );
-			}
-		}*/
-
 		return $buglist;
 	}
 	public function save ( $filename, $buglist )
@@ -108,44 +74,6 @@ class DarkBuglistIO
 			$json_value = json_encode($buglist);
 			file_put_contents( __DIR__ . "/../adata/" . $filename . ".json",$json_value);
 		}
-
-		// Save XML
-		/*{
-			$xml = new DOMDocument();
-			$root = $xml->createElement( "data" );
-			$xml->appendChild( $root );
-
-			foreach ( $buglist AS $bug )
-			{
-				// set ID
-				if ( $bug->id == -1 ) {
-					$bug->id = $lastId;
-					$lastId += 1;
-				}
-				else if ( $bug->id >= $lastId ) {
-					$lastId = $bug->id + 1;
-				}
-
-				// create item
-				$xml_bug = $xml->createElement( "bug" );
-				$xml_bug->setAttribute( "title", $bug->title );
-				$xml_bug->setAttribute( "status", $bug->status );
-				$xml_bug->setAttribute( "id", $bug->id );
-				$xml_bug->appendChild( $xml->createElement( "description", $bug->description ) );
-				// create bug list
-				$xml_bug_history = $xml->createElement( "history" );
-				foreach ( $bug->history AS $bughistory ) {
-					$xml_bug_history->appendChild( $xml->createElement( "info", $bughistory ) );
-				}
-				$xml_bug->appendChild( $xml_bug_history );
-
-				// add item to the xml doc
-				$root->appendChild( $xml_bug );
-			}
-
-			$xml->save( __DIR__ . "/" . $filename . ".xml");
-		}*/
-
 	}
 }
 
--- components/taskstructure.php
+++ components/taskstructure.php
@@ -62,34 +62,6 @@ class DarkTasklistIO
 	public function load ( $filename )
 	{
 		$tasklist = array();
-		/*$xml = new DOMDocument();
-		$xml->load( __DIR__ . "/" . $filename . ".xml" );
-
-		$x = $xml->documentElement;
-		foreach ( $x->childNodes AS $item )
-		{
-			$task = new DarkTask();
-			$task->id = $item->getAttribute( "id" );
-			$task->title = $item->getAttribute( "title" );
-			$task->status = $item->getAttribute( "status" );
-			foreach ( $item->childNodes AS $props )
-			{
-				if ( $props->nodeName == "description" ) {
-					$task->description = $props->nodeValue;
-				}
-				else if ( $props->nodeName == "startdate" ) {
-					$task->startdate = intval($props->nodeValue);
-				}
-				else if ( $props->nodeName == "enddate" ) {
-					$task->enddate = intval($props->nodeValue);
-				}
-				else if ( $props->nodeName == "owner" ) {
-					$task->owner = $props->nodeValue;
-				}
-			}
-			$task->delays = $item->getAttribute( "delays" );
-			array_push( $tasklist, $task );
-		}*/
         $t_filename = __DIR__ . "/../adata/" . $filename . ".json";
 
 		if ( file_exists($t_filename) )
@@ -111,43 +83,6 @@ class DarkTasklistIO
 			$json_value = json_encode($tasklist);
 			file_put_contents( __DIR__ . "/../adata/" . $filename . ".json",$json_value);
 		}
-
-		// Save XML
-		/*{
-			$lastId = 0;
-			$xml = new DOMDocument();
-
-			$root = $xml->createElement( "data" );
-			$xml->appendChild( $root );
-
-			foreach ( $tasklist AS $task )
-			{
-				// set ID
-				if ( $task->id == -1 ) {
-					$task->id = $lastId;
-					$lastId += 1;
-				}
-				else if ( $task->id >= $lastId ) {
-					$lastId = $task->id + 1;
-				}
-
-				// create item
-				$xml_task = $xml->createElement( "task" );
-				$xml_task->setAttribute( "id", $task->id );
-				$xml_task->setAttribute( "title", $task->title );
-				$xml_task->setAttribute( "status", $task->status );
-				$xml_task->setAttribute( "delays", intval($task->delays) );
-				$xml_task->appendChild( $xml->createElement( "description", $task->description ) );
-				$xml_task->appendChild( $xml->createElement( "startdate", intval($task->startdate) ) );
-				$xml_task->appendChild( $xml->createElement( "enddate", intval($task->enddate) ) );
-				$xml_task->appendChild( $xml->createElement( "owner", $task->owner ) );
-
-				// add item to the xml doc
-				$root->appendChild( $xml_task );
-			}
-
-			$xml->save( __DIR__ . "/" . $filename );
-		}*/
 	}
 }
 
